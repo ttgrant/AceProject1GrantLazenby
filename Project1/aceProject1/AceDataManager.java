9aac0704e37e3e17618d1a4d44e4d2fa3ba7becc
ttgrant <grantlazenby@yahoo.com>
2019-09-28 13:09:29 -0500

    acedatamanager tostring left

--- Project1/aceProject1/AceDataManager.java
+++ Project1/aceProject1/AceDataManager.java
@@ -1,10 +1,13 @@
 package aceProject1;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class AceDataManager  implements AceDataManagerADT{
 	private int totalACEPatients;
 	private ArrayList<Patient> AcePatientsList = new ArrayList<Patient>();
+	private String defualtFile = "./aceProject1/PatientFile";
 
 	@Override
 	public void addPatient(PatientADT p) {
@@ -30,20 +33,118 @@ public class AceDataManager  implements AceDataManagerADT{
 
 	@Override
 	public ArrayList<String> getRiskFactors(ArrayList<String> aces) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<String> riskFactors = new ArrayList<String>();
+		if (aces.size() == 1 )
+		{
+			riskFactors.add("Alchohalism");
+		}
+		else if (aces.size() == 2 ) {
+			riskFactors.add("Alchohalism");
+			riskFactors.add("Depression");
+		}
+		else if (aces.size() == 3 ) {
+			riskFactors.add("Alchohalism");
+			riskFactors.add("Depression");
+			riskFactors.add("drug abuse");
+		}
+		else if (aces.size() == 4 ) {
+			riskFactors.add("Alchohalism");
+			riskFactors.add("Depression");
+			riskFactors.add("drug abuse");
+			riskFactors.add("promiscuity");
+			riskFactors.add("chronic lung disease");
+			riskFactors.add("cancer");
+			
+		}
+		else if (aces.size() >= 5) {
+			riskFactors.add("Alchohalism");
+			riskFactors.add("Depression");
+			riskFactors.add("drug abuse");
+			riskFactors.add("promiscuity");
+			riskFactors.add("heart disease");
+			riskFactors.add("chronic lung disease");
+			riskFactors.add("cancer");
+			riskFactors.add("emphysema");
+			riskFactors.add("shortened lifespan");
+			riskFactors.add("stroke");
+			riskFactors.add("diabetes");
+		
+			
+		}
+		return riskFactors;
 	}
 
 	@Override
-	public void writeToFile() {
-		// TODO Auto-generated method stub
-		
+	public void writeToFile()  {
+		try
+		{
+
+			FileWriter fw = new FileWriter(defualtFile);
+			BufferedWriter myOutfile = new BufferedWriter(fw);			
+			
+			for (Patient patient : AcePatientsList) { 
+				Patient pat = patient;
+				
+					myOutfile.write ("Patient: ");
+					myOutfile.write (pat.getName()+"\n");
+					myOutfile.write ("Patient ID: " + pat.getId()+"\n");
+					ArrayList<String> ace = pat.getACEs();
+						for (String string : ace) {
+							myOutfile.write (string + ", ");
+						}
+					myOutfile.write("\n\n");
+			}
+			myOutfile.flush();
+			myOutfile.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Didn't save to " + defualtFile);
+		}
 	}
+	
+	
 
 	@Override
 	public void writeToFile(String fn) {
 		// TODO Auto-generated method stub
 		
+		try
+		{
+
+			FileWriter fw = new FileWriter(fn);
+			BufferedWriter myOutfile = new BufferedWriter(fw);			
+			
+			for (Patient patient : AcePatientsList) { 
+				Patient pat = patient;
+				
+					myOutfile.write ("Patient: ");
+					myOutfile.write (pat.getName()+"\n");
+					myOutfile.write ("Patient ID: " + pat.getId()+"\n");
+					ArrayList<String> ace = pat.getACEs();
+						for (String string : ace) {
+							myOutfile.write (string + ", ");
+						}
+					myOutfile.write("\n\n");
+			}
+			myOutfile.flush();
+			myOutfile.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Didn't save to " + fn);
+		}
 	}
+
+	public String toString()
+	{
+		String toReturn;
+		for (Patient patient : AcePatientsList) {
+			toReturn = patient.toString();
+		}
+		
+		return toReturn;
+	}
+
 
 }
--- Project1/aceProject1/Project1testmain.java
+++ Project1/aceProject1/Project1testmain.java
@@ -4,8 +4,15 @@ public class Project1testmain {
 
 	public static void main(String[] args) {
 		
-		Patient p1 = new Patient();
-		p1.setId("yeeter", "Grant Lazenby");
+		PatientADT p1 = new Patient();
+		p1.setName("Grant Lazenby");
+		p1.setId("1239");
+		PatientADT p2 = new Patient();
+		p2.setName("Katentlyn Lazenby");
+		p2.setId("1238");
+		PatientADT p3 = new Patient();
+		p3.setName("teresa Lazenby");
+		p3.setId("1237");
 		
 		p1.addACE("Physical abuse");
 		p1.addACE("Sexual abuse");
@@ -20,6 +27,12 @@ public class Project1testmain {
 		System.out.println(p1);
 		System.out.println(p1.getName());
 		System.out.println(p1.getId());
+		
+		AceDataManager PM1 = new AceDataManager();
+		PM1.addPatient(p1);
+		PM1.addPatient(p2);
+		PM1.addPatient(p3);
+		PM1.writeToFile("./aceProject1/PatientFile");
 		
 		
 		
